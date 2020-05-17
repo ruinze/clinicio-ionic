5875bb4f19128e1d3a05b0d8765a569888a6b4f4
ruinnze malinao <ruinzemalinao@gmail.com>
2020-05-17 16:04:50 +0800

    test release -- auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'landing',
     pathMatch: 'full'
   },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  // {
+  //   path: 'folder/:id',
+  //   loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  // },
+  // {
+  //   path: 'landing',
+  //   loadChildren: () => import('./pages/landing/landing.module').then( m => m.LandingPageModule)
+  // },
+  // {
+  //   path: 'login',
+  //   loadChildren: () => import('./pages/auth/login/login.module').then( m => m.LoginPageModule)
+  // },
+  // {
+  //   path: 'register',
+  //   loadChildren: () => import('./pages/auth/register/register.module').then( m => m.RegisterPageModule)
+  // },
+  // {
+  //   path: 'dashboard',
+  //   loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  // }
+  { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },  
+  { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
+  
+  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule', canActivate: [AuthGuard] },
+  // { path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard] },
+  // { path: 'list', loadChildren: './list/list.module#ListPageModule', canActivate: [AuthGuard] },
+
 ];
 
 @NgModule({
@@ -19,4 +44,5 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {}
